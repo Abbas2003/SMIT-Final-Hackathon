5eafd5c1c24f81dc830c5b57a328843f4fc9b5d2
Abbas2003 <abbas.mohammad805@gmail.com>
2025-01-26 01:39:30 +0500

    admin setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import Cookies from "js-cookie";
 import Home from './pages/root/home'
 import Login from './pages/auth/Login'
 import Signup from './pages/auth/signup'
 import LoanCalculator from './components/LoanCalculator'
+import Dashboard from './pages/adminDashboard/Dashboard'
 
 function App() {
+  const getUser = () => {
+    const token = Cookies.get("token");
+    const decodedToken = jwt_decode(token);
 
+    console.log("Token:", decodedToken);
+  }
+  getUser();
   return (
    <BrowserRouter>
     <Routes>
@@ -20,7 +28,7 @@ function App() {
       </Route>
 
       {/* Dashboard Routes */}
-      <Route path='/dashboard'>
+      <Route path='/dashboard' element={<Dashboard />}>
         <Route index path="profile" element={<Home />} />
         <Route path="settings" element={<Home />} />
       </Route>
